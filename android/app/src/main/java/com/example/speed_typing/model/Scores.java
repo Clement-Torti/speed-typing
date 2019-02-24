4c8d3eeddaf5d7baece2176ebac01f0d9c26d135
jonathanpoint <33601189+jonathanpoint@users.noreply.github.com>
2019-02-24 14:51:31 +0100

    scoreReader

--- android/app/src/main/java/com/example/speed_typing/model/Scores.java
+++ android/app/src/main/java/com/example/speed_typing/model/Scores.java
@@ -1,28 +1,32 @@
 package com.example.speed_typing.model;
 
+import java.util.Date;
+
 public class Scores {
     private String name;
     private int time;
     private int nbWordWrite;
     private int nbWordFailed;
     private int nbCaractere;
+    private Date date;
 
     /*
     * Le chemin d'acces à l'image du joueur
      */
     private String photoPath;
 
-    public Scores(String name, int time, int nbWordWrite, int nbWordFailed, int nbCaractere, String photoPath) {
+    public Scores(String name, int time, int nbWordWrite, int nbWordFailed, int nbCaractere,Date date, String photoPath) {
         this.name = name;
         this.time = time;
         this.nbWordWrite = nbWordWrite;
         this.nbWordFailed = nbWordFailed;
         this.nbCaractere = nbCaractere;
+        this.date = date;
         this.photoPath = photoPath;
     }
 
-    public Scores(String name, int time, int nbWordWrite, int nbWordFailed, int nbCaractere) {
-        this(name, time, nbWordWrite, nbWordFailed, nbCaractere, "");
+    public Scores(String name, int time, int nbWordWrite, int nbWordFailed, int nbCaractere, Date date) {
+        this(name, time, nbWordWrite, nbWordFailed, nbCaractere,date, "");
     }
 
     /*
--- android/app/src/main/java/com/example/speed_typing/model/Util/ScoreReader.java
+++ android/app/src/main/java/com/example/speed_typing/model/Util/ScoreReader.java
@@ -1,4 +1,69 @@
 package com.example.speed_typing.model.Util;
 
+import android.content.Context;
+
+import com.example.speed_typing.model.Scores;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class ScoreReader {
+    private static ArrayList<Scores> scoreLignes = new ArrayList<Scores>();
+
+    public static ArrayList<Scores> read(String fileName, Context context){
+        String ligne[];
+
+        int nbWordWrite;
+        int nbWordFailed;
+        int nbCharactere;
+        String name;
+        Date date;
+        String photoPath;
+        int time;
+
+        File fichier= new File(fileName);
+
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fichier));
+            ligne = br.readLine().split("-");
+            while (ligne[0] != " ") {
+                name = ligne[0];
+                time = Integer.parseInt(ligne[1]);
+                nbWordWrite = Integer.parseInt(ligne[2]);
+                nbWordFailed= Integer.parseInt(ligne[3]);
+                nbCharactere = Integer.parseInt(ligne[4]);
+                date = dateFormat.parse(ligne[5]);
+                if (ligne.length == 6){
+                    photoPath = ligne[6];
+                    scoreLignes.add(new Scores(name, time, nbWordWrite, nbWordFailed, nbCharactere,date, photoPath));
+                }
+                else
+                    scoreLignes.add(new Scores(name, time, nbWordWrite, nbWordFailed, nbCharactere,date));
+                ligne = br.readLine().split("-");
+            }
+
+            br.close();
+        } catch (FileNotFoundException e) {
+            //FileNotFoundException
+            e.printStackTrace();
+        } catch (IOException e) {
+            //IOException
+            e.printStackTrace();
+        } catch (ParseException e) {
+            //ParseException
+            e.printStackTrace();
+        }
+
+        return scoreLignes;
+
+    }
 }
